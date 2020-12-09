b3362c3d60ef2e3e38c6999ec21e616ebc0cd58d
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-09 14:15:41 +0000

    [feat]: Added backgroud,sprite and movement logic
    Added the background for game, added sprite to change according movement, added movement logic for velocity and gravity

--- core/src/tech/peny/coinman/game/CoinMan.java
+++ core/src/tech/peny/coinman/game/CoinMan.java
@@ -6,28 +6,73 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background;
+	Texture[] man;
+	int manState;
+	int pause = 0;
+	float gravity = 0.2f;
+	float velocity = 0;
+	int manY = 0;
+	ArrayList<Integer> coinX = new ArrayList<Integer>();
+	ArrayList<Integer> coinY = new ArrayList<Integer>();
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
+		man = new Texture[4];
+		man[0] = new Texture("frame-1.png");
+		man[1] = new Texture("frame-2.png");
+		man[2] = new Texture("frame-3.png");
+		man[3] = new Texture("frame-4.png");
+		manY = Gdx.graphics.getHeight()/2;
+
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+
+		if(Gdx.input.justTouched()){
+			velocity = -10;
+		}
+
+
+
+		if(pause < 8){
+			pause++;
+		}else{
+			pause = 0;
+			if(manState < 3){
+				manState++;
+			}else{
+				manState = 0;
+			}
+		}
+
+		velocity += gravity;
+		manY -= velocity;
+		if(manY <= 0){
+			manY = 0;
+		}
+
+
+		batch.draw(man[manState], Gdx.graphics.getWidth() / 2 - man[manState].getWidth()/2,manY );
+
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
